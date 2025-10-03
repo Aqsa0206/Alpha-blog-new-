8e4b01911cfa24d22827b43499a21e3dbcbe1596
Aqsa Zulfiqar <aqsazulfiqar0206@gmail.com>
2025-10-03 19:46:53 +0500

    Add admin user restriction

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
-  def setup
-    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+  setup do
+    @admin_user = User.create(username: "johndoe", email: "johndoe@example.com",
+                              password: "password", admin: true)
+    sign_in_as(@admin_user)
   end
   
   test "get new category form and create category" do
@@ -31,7 +33,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_no_difference 'Category.count' do
       post categories_path, params: { category: { name: " "} }
-      assert_response :redirect
+      assert_response :success
     end
     assert_match "errors", response.body
     assert_select 'div.alert'
