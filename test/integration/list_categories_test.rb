4573f09f5454edf5c8a307434e4fcd060a35182e
Aqsa Zulfiqar <aqsazulfiqar0206@gmail.com>
2025-10-03 19:19:11 +0500

    Build categories index view and integration test

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -4,12 +4,14 @@ class ListCategoriesTest < ActionDispatch::IntegrationTest
 
   def setup
     @category = Category.create(name: "Sports")
-    @category2 = Category.create(name: "programming")
+    # @category2 = Category.create(name: "programming")
+    @category2 = Category.create(name: "Travel")
   end
   
   test "should show categories listing" do
-    get categories_path
-    assert_template 'categories/index'
+    # get categories_path
+    # assert_template 'categories/index'
+    get '/categories'
     assert_select "a[href=?]", category_path(@category), text: @category.name
     assert_select "a[href=?]", category_path(@category2), text: @category2.name
   end
